63f405ae77771cd3b6ad4687d3fff00d6354e598
Eduardo Tomiya <eduardodt@al.insper.edu.br>
2024-01-29 09:28:44 -0300

    SORTEIA IMAGENS

--- games_creen.py
+++ games_creen.py
@@ -1,16 +1,48 @@
 import pygame
 from config import FPS, WIDTH, HEIGHT, BLACK
 from assets import carrega_arquivos
+import random
+
+def sorteia_imagem(sheepDog_images, mop_images):
+    # Escolhe aleatoriamente entre Cachorro e Mop
+    eh_cachorro = random.choice([True, False])
+    imagem = random.choice(sheepDog_images if eh_cachorro else mop_images)
+
+    # Sorteia a posição x (garantindo que a imagem não saia da tela)
+    x = random.randint(0, WIDTH - imagem.get_width())
+
+    # Sorteia uma posição y negativa para a imagem aparecer gradualmente
+    y = random.randint(-100, -1)
+
+    # Sorteia uma velocidade aleatória
+    velocidade = random.randint(1, 5)
+
+    return {
+        'imagem': imagem,
+        'eh_cachorro': eh_cachorro,
+        'x': x,
+        'y': y,
+        'velocidade': velocidade
+    }
+
 
 def game_screen(window):
     # Variável para o ajuste de velocidade
     clock = pygame.time.Clock()
 
     dicionario_de_arquivos = carrega_arquivos()
-
+    sheepDog_images = dicionario_de_arquivos['sheepDog']
+    mop_images = dicionario_de_arquivos['Mop']
     DONE = 0
     PLAYING = 1
     state = PLAYING
+    # Lista para armazenar as imagens sorteadas
+    imagens_sorteadas = []
+
+    # Sorteando 5 imagens
+    for _ in range(5):
+        imagem_sort = sorteia_imagem(sheepDog_images, mop_images)
+        imagens_sorteadas.append(imagem_sort)
 
     # ===== Loop principal =====
     while state != DONE:
@@ -23,7 +55,12 @@ def game_screen(window):
                 state = DONE
 
         # ----- Gera saídas
-        window.fill(BLACK)  # Preenche com a cor branca
+        window.fill(BLACK)  # Preenche com a cor preta
+
+        # Desenha as imagens sorteadas na tela
+        for img in imagens_sorteadas:
+            img['y'] += img['velocidade']  # Atualiza a posição y
+            window.blit(img['imagem'], (img['x'], img['y']))
 
         pygame.display.update()  # Mostra o novo frame para o jogador
 
